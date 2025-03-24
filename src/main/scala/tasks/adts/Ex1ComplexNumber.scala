51dc97716ebf281364cef16503b5359e845c29fd
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2025-03-24 15:40:18 +0100

    Implemented task on complex numbers ADT

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -18,13 +18,31 @@ object Ex1ComplexNumbers:
       def asString(): String
 
   object BasicComplexADT extends ComplexADT:
+    case class ComplexNumber(re: Double, im: Double)
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = ComplexNumber
+
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
+
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case ComplexNumber(re, _) => re
+
+      def im(): Double = complex match
+        case ComplexNumber(_, im) => im
+
+      def sum(other: Complex): Complex = complex match
+        case ComplexNumber(re, im) => ComplexNumber(re + other.re, im + other.im)
+
+      def subtract(other: Complex): Complex = complex match
+        case ComplexNumber(re, im) => ComplexNumber(re - other.re, im - other.im)
+
+      def asString(): String = complex match
+        case ComplexNumber(re, im) =>
+          (if im == 0 || re != 0 then re.toString else "") +
+            (if im > 0 && re != 0 then " + " else if re == 0 || im == 0 then "" else " - ") +
+            (if im > 0 then im.toString + "i"
+            else if im < 0 && re != 0 then math.abs(im) + "i"
+            else if re == 0 && im != 0 then im + "i"
+            else "")
