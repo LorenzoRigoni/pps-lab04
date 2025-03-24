8d26d29843417f22f01cae21eecaf5bee1494b28
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2025-03-24 17:47:51 +0100

    Implemented summables task

--- src/main/scala/tasks/typeclasses/Ex4Summables.scala
+++ src/main/scala/tasks/typeclasses/Ex4Summables.scala
@@ -1,6 +1,8 @@
 package u04lab
-import u03.Sequences.* 
+import u03.Sequences.*
 import Sequence.*
+
+import scala.annotation.tailrec
 
 /*  Exercise 4: 
  *  - Complete the implementation of ad-hoc polymorphic sumAll, using summable.sum and summable.zero
@@ -20,21 +22,29 @@ object Ex4Summables:
 
   def sumAll[A: Summable](seq: Sequence[A]) =
     val summable = summon[Summable[A]]
-    ???  // complete here
+    @tailrec
+    def _sum(acc: A, s: Sequence[A]): A = s match
+      case Cons(h, t) => _sum(summable.sum(acc, h), t)
+      case _          => acc
+    _sum(summable.zero, seq)
 
   given Summable[Int] with
     def sum(a1: Int, a2: Int): Int = a1 + a2
     def zero: Int = 0
   
   // write givens for Summable[Double] and Summable[String]
+  given Summable[Double] with
+    def sum(a1: Double, a2: Double): Double = a1 + a2
+    def zero: Double = 0.0
+
+  given Summable[String] with
+    def sum(a1: String, a2: String): String = a1 + a2
+    def zero: String = ""
 
   @main def trySummables =
     val si = Cons(10, Cons(20, Cons(30, Nil())))  
     println:
       sumAllInt(si) // 60
-
-    /* uncomment from here   
-
     println:
       sumAll(si) // 60
 
@@ -45,6 +55,3 @@ object Ex4Summables:
     val ss = Cons("10", Cons("20", Cons("30", Nil())))  
     println:
       sumAll(ss) // "102030"
-
-    */  
-
