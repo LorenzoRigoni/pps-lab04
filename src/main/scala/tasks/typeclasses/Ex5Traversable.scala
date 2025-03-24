ce1425c8f4a3997dfcd1c7b5dc10cfbdabfbd1e8
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2025-03-24 21:51:42 +0100

    Implemented task on traversables

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,11 @@
 package u04lab
-import u03.Sequences.* 
+
+import u03.Sequences.*
 import Sequence.*
+import u03.Optionals.Optional.Just
+import u03.extensionmethods.Optionals.*
+import Optional.*
+import u03.Optionals
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -16,11 +21,29 @@ import Sequence.*
  */
 
 object Ex5Traversable:
+  trait Traversable[T[_]]:
+    def forEach[A](t: T[A])(f: A => Unit): Unit
 
-  def log[A](a: A): Unit = println("The next element is: "+a)
+  given Traversable[Sequence] with
+    def forEach[A](seq: Sequence[A])(f: A => Unit): Unit = seq match
+      case Cons(h, t) => f(h); forEach(t)(f)
+      case _ => ()
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  given Traversable[Optional] with
+    def forEach[A](opt: Optional[A])(f: A => Unit): Unit = opt match
+      case Optional.Just(v) => f(v)
+      case _ => ()
 
-  
+  def log[A](a: A): Unit = println("The next element is: " + a)
+
+  def logAll[T[_], A](t: T[A])(using trav: Traversable[T]): Unit = trav.forEach(t)(log)
+
+  @main def traversableMain(): Unit =
+    val seq = Cons(10, Cons(20, Cons(30, Nil())))
+    val opt: Optional[Int] = Optional.Just(30)
+
+    println("Test logAll of Sequence, expected 'The next element is: 10, 20, 30'")
+    logAll(seq)
+
+    println("Test logAll of Optional, expected 'The next element is: 30'")
+    logAll(opt)
